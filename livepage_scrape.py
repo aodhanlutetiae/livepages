cb9cf4cdd341d79b25783300f10fcda3f0f44159
aodhanlutetiae <39188450+aodhanlutetiae@users.noreply.github.com>
2026-05-18 16:43:41 +0100

    Refactor livepage_scrape.py for single data scrape
    Refactored livepage_scrape.py to remove the loop and directly scrape data once. Added functionality to write scraped data to a CSV file.

--- livepage_scrape.py
+++ livepage_scrape.py
@@ -2,24 +2,18 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from datetime import datetime as dt
-from time import sleep
 import csv
 
 # key info
 livepage_address = "https://www.bbc.co.uk/news/live/c99l70zy1j7t"
 my_headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:84.0) Gecko/20100101 Firefox/84.0"}
 
-visits = 0
+req = requests.get(livepage_address, headers = my_headers)
+soup = bs(req.content, "html.parser")
+for x in soup.find('span', class_ = "ssrcss-798imn-CounterStringContainer e1naroyk0"):
+    d = x.text
+    timenow = str(dt.now())
+    with open("datafiles/bbc_live_data.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([d, timenow])
 
-# set to run every minute for 5 hours
-while visits < 300:
-    req = requests.get(livepage_address, headers = my_headers)
-    soup = bs(req.content, "html.parser")
-    for x in soup.find('span', class_ = "ssrcss-798imn-CounterStringContainer e1naroyk0"):
-        d = x.text
-        timenow = str(dt.now())
-        with open("datafiles/bbc_live_data.csv", "a", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow([d, timenow])
-    visits = visits+1
-    sleep(60)
